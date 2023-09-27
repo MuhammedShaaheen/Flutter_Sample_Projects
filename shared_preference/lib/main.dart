02d578b74ec24d574b74feb448251934aa572797
Muhammed Shaheen <84826506+MuhammedShaaheen@users.noreply.github.com>
2023-09-27 15:25:30 +0530

    1

---
+++ shared_preference/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preference/home_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shared Preferences',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ shared_preference/lib/profile_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text('Value Found'),
+          ],
+        ),
+      ),
+    );
+  }
+}
