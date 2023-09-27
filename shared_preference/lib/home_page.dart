8dd489ac7d267b314cfb5d5a7630ffb24e39e07c
Muhammed Shaheen <84826506+MuhammedShaaheen@users.noreply.github.com>
2023-09-27 16:52:51 +0530

    2

--- shared_preference/lib/home_page.dart
+++ shared_preference/lib/home_page.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preference/profile_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    getSavedData(context);
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          TextFormField(),
-          ElevatedButton(onPressed: () {}, child: Text('Save Value'))
-        ],
-      )),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _textController,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  saveDataToStorage();
+                },
+                child: Text('Save Value'))
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<void> saveDataToStorage() async {
+    print(_textController.text);
+
+    // Set an Object for Shared Preferences
+
+    final sP = await SharedPreferences.getInstance();
+
+    // Save Value
+
+    await sP.setString('saved_value', _textController.text);
+  }
+
+  Future<void> getSavedData(BuildContext context) async {
+    final sP = await SharedPreferences.getInstance();
+    final savedValue = sP.getString('saved_value');
+    if (savedValue != null) {
+      Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
+        return ProfilePage();
+      }));
+    }
   }
 }
